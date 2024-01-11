118ec3f85a4b9c71e9de0feeb097a03eaaf5cdca
Shaun Lee <shaun.lee@gmail.com>
2024-01-10 22:58:26 -0800

    Have magic-vlsi depend on tcl-tk-with-x

--- Formula/magic-vlsi.rb
+++ Formula/magic-vlsi.rb
@@ -1,8 +1,8 @@
 class MagicVlsi < Formula
   desc "VLSI layout tool written in Tcl"
-  version "8.3.456"
   homepage "http://opencircuitdesign.com/magic/"
-  url "https://github.com/RTimothyEdwards/magic/archive/refs/tags/#{version}.tar.gz"
+  url "https://github.com/RTimothyEdwards/magic/archive/refs/tags/8.3.456.tar.gz"
+  version "8.3.456"
   sha256 "ef17c343c89ac54699f87f6c853ec7e4814f322734bd3b54a157a7d95cab905a"
   license "MIT"
 
@@ -16,62 +16,13 @@ class MagicVlsi < Formula
   depends_on "libxi"
   depends_on "libxmu"
   depends_on "libxrender"
-  depends_on "python3"
-
   depends_on :macos
-
-  resource "tcl" do
-    url "https://downloads.sourceforge.net/project/tcl/Tcl/8.6.13/tcl8.6.13-src.tar.gz"
-    sha256 "43a1fae7412f61ff11de2cfd05d28cfc3a73762f354a417c62370a54e2caf066"
-  end
-
-  resource "tk" do
-    url "https://downloads.sourceforge.net/project/tcl/Tcl/8.6.13/tk8.6.13-src.tar.gz"
-    mirror "https://fossies.org/linux/misc/tk8.6.13-src.tar.gz"
-    sha256 "2e65fa069a23365440a3c56c556b8673b5e32a283800d8d9b257e3f584ce0675"
-  end
+  depends_on "python3"
+  depends_on "shaunplee/asic/tcl-tk-with-x"
 
   def install
     ENV.deparallelize
-
-    resource("tcl").stage do
-      tcl_tk_args = %W[
-        --prefix=#{prefix}/tcl-tk
-        --enable-threads
-        --enable-64bit
-      ]
-
-      cd "unix" do
-        system "./configure",
-               *tcl_tk_args
-        system "make"
-        system "make", "install"
-        system "make", "install-private-headers"
-      end
-
-      ENV.prepend_path "PATH", bin
-
-      resource("tk").stage do
-        cd "unix" do
-          system "./configure",
-                 "--with-tcl=#{prefix}/tcl-tk/lib",
-                 "--enable-xss=no",
-                 "--with-x",
-                 "--x-includes=/opt/X11/include",
-                 "--x-libraries=/opt/X11/lib",
-                 "CFLAGS=-I/opt/X11/include",
-                 *tcl_tk_args
-
-          inreplace "Makefile",
-                    /^LIB_RUNTIME_DIR[^\n]*$/,
-                    "LIB_RUNTIME_DIR = $(libdir)"
-
-          system "make"
-          system "make", "install"
-          system "make", "install-private-headers"
-        end
-      end
-    end
+    tcltk = Formula["shaunplee/asic/tcl-tk-with-x"]
 
     # magic crashes on start with a BadMatch error:
     ### X Error of failed request:  BadMatch (invalid parameter attributes)
@@ -79,9 +30,8 @@ class MagicVlsi < Formula
     ### Minor opcode of failed request:  27 (X_GLXCreatePbuffer)
     # So let's set --with-opengl=no
     system "./configure",
-           "--prefix=#{prefix}",
-           "--with-tcl=#{prefix}/tcl-tk/lib",
-           "--with-tk=#{prefix}/tcl-tk/lib",
+           "--with-tcl=#{tcltk.opt_prefix}",
+           "--with-tk=#{tcltk.opt_prefix}",
            "--x-includes=/opt/X11/include",
            "--x-libraries=/opt/X11/lib",
            "--with-opengl=no", # disable OpenGL
