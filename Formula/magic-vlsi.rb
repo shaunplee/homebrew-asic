a484af2f0e4c2d12036880436f9a75c8b00e11f2
Shaun Lee <shaun.lee@gmail.com>
2025-02-25 03:36:16 +0000

    magic-vlsi 8.3.522

--- Formula/magic-vlsi.rb
+++ Formula/magic-vlsi.rb
@@ -3,8 +3,8 @@ class MagicVlsi < Formula
 
   desc "VLSI layout tool written in Tcl"
   homepage "http://opencircuitdesign.com/magic/"
-  url "https://github.com/RTimothyEdwards/magic/archive/refs/tags/8.3.521.tar.gz"
-  sha256 "a54c669f9c6499f717a552127ba56cdb51296ccb0bd03cfd99f0e9be260bcc36"
+  url "https://github.com/RTimothyEdwards/magic/archive/refs/tags/8.3.522.tar.gz"
+  sha256 "230802af0c91779d2aa692d39a64f90fb55db29fbd2d3d42c46f54fe531312ad"
   license "MIT"
 
   livecheck do
